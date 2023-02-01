08a96de5b82f0b814e4a64b0db4f4f464bba8a61
JrSouzaCardoso <jrcardoso541@gmail.com>
2023-02-01 00:23:12 -0300

    updatede settings of the environment: adde and sttings the apache-srv with php8, mysql-srv and phpmyadmin-srv, all in containers docker

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <section class="area-login">
+        <div class="login">
+            <div class="title">Entrar</div>
+            <form method="POST">
+                <input type="text" name="email" placeholder="Email" autofocus>
+                <input type="password" name="senha" placeholder="Senha">
+                <label><input type="checkbox">Lembre me</label>
+                <input type="submit" value="entrar">
+            </form>
+            <p>Controle de Chaves - Infra</p>
+            <div><img src="img/logo-scmba-azul.png"></div>
+        </div>
+    </section>
+</body>
+</html>
