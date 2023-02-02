6d1c423d57e2ca0a122e7ba2fc602dc7fa8ad27b
JrSouzaCardoso <jrcardoso541@gmail.com>
2023-02-01 23:45:55 -0300

    implamented conection with data base

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
+    include("conexao.php");
 
+    if(isset($_POST['button-login'])){
+
+        if(strlen($_POST['email']) == 0){
+            echo "preencha o seu email";
+        }else if(strlen($_POST['senha']) == 0){
+            echo "preencha a sua senha";
+        }else{
+
+            $email = $mysqli->real_escape_string($_POST['email']);
+            $senha = $mysqli->real_escape_string($_POST['senha']);
+        
+        }
+
+    }
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +31,7 @@
                 <input type="text" name="email" placeholder="Email" autofocus>
                 <input type="password" name="senha" placeholder="Senha">
                 <label><input type="checkbox">Lembre me</label>
-                <input type="submit" value="entrar">
+                <input type="submit" name="button-login" value="entrar">
             </form>
             <p>Controle de Chaves - Infra</p>
             <div><img src="img/logo-scmba-azul.png"></div>
