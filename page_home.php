92fdb4eaba9b0961175b4819f58316d473a91887
JrSouzaCardoso <jrcardoso541@gmail.com>
2023-02-07 18:32:56 -0300

    implemented general update in page home and your style

--- page_home.php
+++ page_home.php
@@ -1,106 +1,91 @@
 <?php
 
-    include("validations/validate-session.php");
+  include('./validations/validate-session.php');
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/styleHome.css">
-    <title>Página inicial</title>
+  <meta charset="UTF-8">
+  <title>Página de cadastro </title>
+  <link rel="stylesheet" href="./css/style-header.css">
+  <link rel="stylesheet" href="./css/style-article.css">
 </head>
+
 <body>
-    <div class="page">
-        <div class="colum-1">
-            <div>
-                <form action="logout.php">
-                    <button on class="btn-sair" type="submit">Sair</button>
-                </form>
-            </div>
-            <div>
-                <form action="logout.php">
-                    <button class="button" type="submit">Chaves</button>
-                </form>
-            </div>
-            <div>
-                <form action="logout.php">
-                    <button class="button" type="submit">Histórico</button>
-                </form>
-            </div>
-        </div>
-        <div class="colum-2">
-            <h2>Bem Vindo, <?php echo $_SESSION['nome']; ?>.</h2>
-            <div class="content">
-                <h1>Registrar Emprestimo de Chave 🔑</h1>
-                <form id="form">
-                    <div>
-                        <input type="text" placeholder="Número da Chaves" class="inputs required" oninput="nameValidate()">
-                        <span class="span-required">Nome deve ter no mínimo 3 caracteres</span>
-                    </div>
-                    <div>
-                        <input type="email" placeholder="Digite seu melhor email" class="inputs required" oninput="emailValidate()">
-                        <span class="span-required">....</span>
-                    </div>
-                    <div>
-                        <input type="password" placeholder="Senha" class="inputs required" oninput="mainPasswordValidate()">
-                        <span class="span-required">....</span>
-                    </div>
-                    <div>
-                        <input type="password" placeholder="Senha" class="inputs required" oninput="mainPasswordValidate()">
-                        <span class="span-required">....</span>
-                    </div>
+  <header id="header">
+    <img class="img-home" src="img/logo-scmba-branca.png">
+    <h1>Olá, <?php echo $_SESSION['nome']; ?></h1>
+    <nav id="nav">
+      <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">Menu
+        <span id="hamburger"></span>
+      </button>
+      <ul id="menu" role="menu">
+        <li><a href="/">Cadastro</a></li>
+        <li><a href="/">Histórico</a></li>
+        <li><a href="/">Chaves</a></li>
+        <li><a class="sair" href="./logout.php">Sair</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <article>
+      <div class="content">
+          <h1>Registrar Emprestimo de Chave 🔑</h1>
+          <div>
+              <div class="box-select">
+                  <form>
+                      <input type="radio" name="consulta" checked value="1">
+                      <label for="">Colaborador</label>
+                      <input type="radio" name="consulta" value="2">
+                      <label for="">Prestador</label>
+                      <input type="radio" name="consulta" value="3">
+                      <label for="">Outros</label>
+                  </form>
+              </div>
+              <div id="colaborador">
+                  <div class="inputs">
+                      <div class="input">
+                          <input type="text" required>
+                          <span>Número</span>
+                      </div> 
+                      <div class="input">
+                          <input type="text" required>
+                          <span>Matrícula</span>
+                      </div>
+                      <div class="input">
+                          <input id="date" required type="datetime-local" value="<?php date_default_timezone_set("America/Recife"); echo date("Y-m-d H:i");?>">
+                          <span>Data / Hora</span>
+                      </div>
+                  </div>
+              </div>
+              <div id="prestador" hidden>
+                  DIV 2
+              </div>
+              <div id="outros" hidden>
+                  DIV 3
+              </div>
+              
+              <div class="title-textarea">
+                <p>Motivo / Observação</p>
+              </div>
+              
+              <div class="textarea">
+                  <textarea required cols="25" rows="10"></textarea>
+              </div>
+              
+          </div>
+
+          <button class="button" type="submit">Enviar</button>
+      </div>
+  </article>
 
 
-                    <div>
-                        <div>
-                        <form>
-                            <input type="radio" name="consulta" checked value="1">Colaborador
-                            <input type="radio" name="consulta" value="2">Prestador
-                            <input type="radio" name="consulta" value="3">Outros
-                        </form>
-                        </div>
-                        <div id="colaborador">
-                            <div class="colum2-lim1">
-                                <div class="input">
-                                    <input type="text" required="required">
-                                    <span>Número da Chave</span>
-                                </div> 
-                                <div class="input">
-                                    <input type="text" required="required">
-                                    <span>Matrícula do Comodatário</span>
-                                </div>
-                                <div class="date">
-                                    <p for="date">Data / Hora:</p>
-                                    <input id="date" type="datetime-local" value="<?php date_default_timezone_set("America/Recife"); echo date("Y-m-d H:i");?>">
-                                </div>  
-                            </div>
-                        </div>
-                        <div id="prestador" hidden>
-                            DIV 2
-                        </div>
-                        <div id="outros" hidden>
-                            DIV 3
-                        </div>
-                    </div>
-
-                    
-                    
-
-                    
-
-                    <textarea class="inputs" name="descricao" id="descricao" cols="25" rows="10" placeholder="Fale um pouco sobre você..."></textarea>
-            
-                    <button class="button" type="submit">Enviar</button>
-                </form>
-            </div>
-        </div>
-        <div class="colum-3">
-            <img class="img-home" src="img/logo-scmba-branca.png"></div>
-        </div>
-    </div>
+  
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="scrip-consulta.js"></script>
+<script src="./script.js"></script>
 </html>
